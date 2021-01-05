ecdfa7af00562ec9364c7be6d697ee4b11e82585
YoungHaChoi <askillofgod159@naver.com>
2021-01-05 10:59:46 +0900

    commit

--- src/array/NumberArray.java
+++ src/array/NumberArray.java
@@ -7,24 +7,21 @@ public class NumberArray {
         int a,b,c = 0;
         int count = 0;
         int result = 0;
-        int [] array = new int[10];
+
+        // 내일은 버퍼리더 이용해서 돌려보기
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
+        result = (sc.nextInt() * sc.nextInt() * sc.nextInt());
+        String str = Integer.toString(result);
+        sc.close();
 
-        result = a * b * c;
-
-        //BufferReader 사용법, ChatAt에 -'0'을 해야하는 이유에 대해서 구글링
-
-        for(int i = 0; i<=9; i++){
-            for(int j = 0; j<=9; j++){
-                if(array[i] == j){
+        for(int i = 0; i<10; i++){
+            count = 0;
+            for(int j = 0 ; j < str.length() ;j++){
+                if((str.charAt(j) - '0') == i){
                     count++;
                 }
             }
+            System.out.println(count);
         }
-
-
     }
 }
