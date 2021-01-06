973be1ed86b3d197e585ee3ba5cbde5272f75210
YoungHaChoi <askillofgod159@naver.com>
2021-01-06 19:54:04 +0900

    commit

--- src/array/NumberArray.java
+++ src/array/NumberArray.java
@@ -1,18 +1,23 @@
 package array;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class NumberArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int a,b,c = 0;
         int count = 0;
         int result = 0;
 
-        // 내일은 버퍼리더 이용해서 돌려보기
-        Scanner sc = new Scanner(System.in);
-        result = (sc.nextInt() * sc.nextInt() * sc.nextInt());
-        String str = Integer.toString(result);
-        sc.close();
+        // 버퍼리더를 이용해서 알고리즘 작동시간 단축
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        result = Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine())
+    * Integer.parseInt(bf.readLine());
+        String str = String.valueOf(result);
+
 
         for(int i = 0; i<10; i++){
             count = 0;
