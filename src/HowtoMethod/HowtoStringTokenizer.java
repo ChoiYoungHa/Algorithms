f4cb71b42762f5ba6957d0a38b7ee4214a85e38e
YoungHaChoi <askillofgod159@naver.com>
2021-01-13 19:33:47 +0900

    commit

---
+++ src/HowtoMethod/HowtoStringTokenizer.java
@@ -0,0 +1,34 @@
+package HowtoMethod;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HowtoStringTokenizer {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        //StringTokenizer를 사용할 땐 생성자라서 객체를 넣고 어떤식으로 분리할지 정해주어야한다.
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ",true);
+
+        // 토큰이 몇개인지 모를때 있을땐 true로 지정하고 출력
+        while(st.hasMoreTokens()){
+            String data = st.nextToken();
+            if(data.equals(" ")){
+                System.out.println("!");
+            }
+            else{
+                System.out.println(data);
+            }
+        }
+
+        // 토큰수를 세어 for문으로 출력
+        /*int count = st.countTokens();
+        for(int i = 0; i < count; i++){
+            System.out.println(st.nextToken());
+        }*/
+
+
+    }
+}
--- src/array/MaybeAvg.java
+++ src/array/MaybeAvg.java
@@ -3,36 +3,47 @@ package array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class MaybeAvg {
     public static void main(String[] args) throws IOException {
-        // 내일 다시 풀이
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int [] array;
-        int num = Integer.parseInt(bf.readLine());
 
-        for(int i =0; i < num; i++){
-            st = new StringTokenizer(bf.readLine()," ");
-            int n = Integer.parseInt(st.nextToken());
-            array = new int [n];
+        // 먼저, 몇개 구할 것인지 받고,
+        int n = Integer.parseInt(bf.readLine());
+        // 구할 것 만큼 반복하고
+        for(int i = 0; i < n; i++){
+            //1 50 20 30 50 이런식으로 입력들올테니 " "로 토큰 구분
+            st = new StringTokenizer(bf.readLine(), " ");
+
+            //첫번째로 받은 값은 배열의 숫자이니, 그만큼 배열선언
+            int num = Integer.parseInt(st.nextToken());
+            int [] array = new int [num];
             double hap = 0;
-            for(int k = 0; k < array.length; k++){
+
+            //받은 수 만큼 반복할거고 첫번째 토큰 다음인 두번째 토큰부터 array[k]에 저장
+            //그리고 전체 점수 합산
+            for(int k = 0; k < num; k++){
                 array[k] = Integer.parseInt(st.nextToken());
                 hap += array[k];
             }
+            // 평균을 구하고
+            double avg = hap/num;
+            double count = 0;
 
-            double avg = hap/n;
-            int count = 0;
-            for(int j = 0; j < n ; j++){
-                if(avg < array[j]){
+            // 평균보다 점수가 높으면 count 1증가
+            for(int j = 0; j < num ; j++){
+                if(array[j] > avg){
                     count++;
                 }
             }
-            System.out.printf(".%3f\n", (count / n) * 100);
+
+            //평균보다 점수가 높은 학생의 비율을 구하는 문제이니,
+            //평균이상인 학생 / 전체학생 * 100을하면 비율이 계산 됨.
+            System.out.printf("%.3f%%\n", (count/num) * 100);
         }
+
     }
 }
 
