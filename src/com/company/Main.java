bddba9ddddc2fd6511162be98ba96cac750405f1
YoungHaChoi <askillofgod159@naver.com>
2021-01-19 17:11:55 +0900

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a == 0){
+        int b = sc.nextInt();
+
+
+        if(a == 1 && b == 1){
             System.out.println(1);
         }
         else{
