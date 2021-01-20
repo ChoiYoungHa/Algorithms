bc0371097fc79b084198438675b6aa18ff5c1218
YoungHaChoi <askillofgod159@naver.com>
2021-01-20 18:49:23 +0900

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,13 +13,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-
-        if(a == 1 && b == 1){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(0);
+        System.out.println(a > b ? a:b);
         }
     }
-}
