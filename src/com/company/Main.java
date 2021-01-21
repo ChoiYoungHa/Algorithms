0661393ef9e4c2c06aff20952afa9effb7d3dabf
YoungHaChoi <askillofgod159@naver.com>
2021-01-21 09:48:59 +0900

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,13 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-//코드업 입출력 문제
+//코드업 삼항 연산자 문제 최소값 구하기
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a > b ? a:b);
+        int c = sc.nextInt();
+        System.out.println((a < b ? a:b) < c ? (a < b ? a:b) : c);
         }
     }
