14e7b036875dcd12f7054ff6e00bcd96eb3b38d0
YoungHaChoi <askillofgod159@naver.com>
2021-01-22 10:16:02 +0900

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,22 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 //코드업 삼항 연산자 문제 최소값 구하기
+//코드업 1065번 문제
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        System.out.println((a < b ? a:b) < c ? (a < b ? a:b) : c);
+
+        int [] array = new int [3];
+        for(int i = 0; i < array.length; i++){
+            array[i] = sc.nextInt();
+            if(array[i] % 2 == 0){
+                System.out.println("even");
+            }
+            else if(array[i] % 2 == 1){
+                System.out.println("odd");
+            }
+        }
+
+
         }
     }
