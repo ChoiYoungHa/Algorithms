fbf13f31a8c35e8368813c18064d5373d2231ec7
YoungHaChoi <askillofgod159@naver.com>
2021-04-14 20:29:13 +0900

    recur

---
+++ src/sample/Recur.java
@@ -0,0 +1,21 @@
+package sample;
+
+//산업기사 재귀함수 예제
+public class Recur {
+    public static void main(String[] args) {
+        int n;
+        n = recur(7);
+        System.out.println();
+        System.out.printf("%d",n);
+    }
+
+    public static int recur(int n) {
+        System.out.println(n);
+        if (n < 1) {
+            return 2;
+        }
+        else {
+            return 3 * recur(n - 1) + 1;
+        }
+    }
+}
