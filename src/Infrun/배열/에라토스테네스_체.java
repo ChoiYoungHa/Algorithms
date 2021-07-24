b88354df46404cc24a23fb2bd0588a76fa0e7102
YoungHaChoi <askillofgod159@naver.com>
2021-07-24 21:19:05 +0900

    merge

--- src/Infrun/배열/에라토스테네스_체.java
+++ src/Infrun/배열/에라토스테네스_체.java
@@ -2,6 +2,7 @@ package Infrun.배열;
 
 import java.util.Scanner;
 
+
 public class 에라토스테네스_체 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,6 +14,7 @@ public class 에라토스테네스_체 {
     }
 
     // 2중 for문에서 i랑 j가 같아야 배수를 처리한다.
+    // 다른방법의 배수 알고리즘
     static int solution(int [] array, int n) {
         int answer = 0;
         for (int i = 2; i <= n; i++){
