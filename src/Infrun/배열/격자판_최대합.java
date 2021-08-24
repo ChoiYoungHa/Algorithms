869fa96d2bfdf14528ed6ba9452d1aeb3e23daa8
YoungHaChoi <askillofgod159@naver.com>
2021-08-24 23:12:08 +0900

    격차판_최대합

--- src/Infrun/배열/격자판_최대합.java
+++ src/Infrun/배열/격자판_최대합.java
@@ -13,42 +13,34 @@ public class 격자판_최대합 {
                 array[i][j] = sc.nextInt();
             }
         }
-        solution(array, n);
+        System.out.println(solution(array, n));
     }
 
     static int solution(int [] [] array, int n) {
         int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+
         for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
             for (int j = 0; j < n; j++) {
-                if (array[i][j] > answer) {
-                    answer = array[i][j];
-                }
+                sum1 += array[i][j];
+                sum2 += array[j][i];
             }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (array[j][i] > answer) {
-                    answer = array[j][i];
-                }
-            }
+        sum1 = 0;
+        sum2 = 0;
+        for (int i = 0; i < n; i++){
+            sum1 += array[i][i];
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < i; j++) {
-                if (array[i][j] > answer) {
-                    answer = array[i][j];
-                }
-            }
+        for (int j = 0; j < n; j++){
+            sum2 += array[j][n - j - 1];
         }
-
-        for (int i = n - 1; i > 0; i--) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (array[i][j] > answer) {
-                    answer = array[i][j];
-                }
-            }
-        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
 
         return answer;
     }
